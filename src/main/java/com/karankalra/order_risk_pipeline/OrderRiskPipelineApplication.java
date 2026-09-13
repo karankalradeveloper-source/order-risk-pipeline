20cbe078455013d86a76e3d6609121a0a14df8c1
Karan Kalra <karankalradeveloper@gmail.com>
2026-09-14 01:28:05 +0530

    Initial project setup: Spring Boot, Postgres via Docker Compose

---
+++ src/main/java/com/karankalra/order_risk_pipeline/OrderRiskPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.karankalra.order_risk_pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderRiskPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderRiskPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karankalra/order_risk_pipeline/OrderRiskPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karankalra.order_risk_pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderRiskPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
